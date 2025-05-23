0d0d2de027907fe5ef448fef725206608da0db98
NERD777 <nugrohoandi343@gmail.com>
2025-05-23 17:49:51 +0700

    feat and fix: refactor contract, integration contract into frontend, add marketplace

---
+++ contract-addresses.ts
@@ -0,0 +1,19 @@
+/**
+ * Contract addresses for the SEI Testnet
+ * Updated from deployment on April 26, 2024
+ */
+export const contractAddresses = {
+  // Token contracts
+  usdc: '0xDb8F9c652f613FAdB680daE048642D0e6AC8F733',
+  principalToken: '0x635f07BaB844533557dB2CC7bf8F515371e884E1',
+  yieldToken: '0x62D7c9a5462a69aC2e59B96DfdC3572b3f365f3e',
+
+  // Core protocol contracts
+  treasury: '0xEae72AC3F92D846905e5317E6CEb1Ae92Df08CDc',
+  yieldProtocol: '0xDFd5D5b60CDD090156E774d4D97D9a778a50Cfbe',
+  fundsVault: '0x4A4c34dCfACEf3190907653DE8ba3d8D992Dc178',
+
+  // Auxiliary contracts
+  faucet: '0xE6C120e6dB3E3c6af624b42617AD5922E1Bda681',
+  merchant: '0xF88A7306b8edCc6Ffa48bF0EA1F0f530a6bC1D30',
+}
